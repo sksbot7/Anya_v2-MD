404d78bb9d7735022dfbfe90d2421848f3ba4189
sksbot7 <104474096+sksbot7@users.noreply.github.com>
2024-11-18 22:06:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,40 +8,40 @@ module.exports = {
     /**
      * bot details and parameters
      */
-    botname: process.env.BotName || "Qᴜᴇᴇɴ ᴀɴʏᴀ ᴠ2",
-    footer: process.env.Footer || "© 𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 𝐐𝐮𝐞𝐞𝐧 𝐀𝐧𝐲𝐚 𝐁𝐨𝐭",
+    botname: process.env.BotName || "ㅤ𝗦𝗞𝗦 𝙱ᷤ𝙾ᷜ𝚃ᷤ",
+    footer: process.env.Footer || "© 𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 S Y 4 M",
     prefa: process.env.Prefix || "-",
-    themeemoji: process.env.ThemeEmoji || "🎐",
+    themeemoji: process.env.ThemeEmoji || "🌝",
 
     /**
      * owner details and parameters
      */
-    ownername: process.env.Owner_Name || "Pika~Kun",
-    ownernumber: process.env.Owner_Number || "916900904828",
-    instagramId: process.env.Insta || "3.69_pika",
+    ownername: process.env.Owner_Name || "S Y 4 M",
+    ownernumber: process.env.Owner_Number || "919888280858",
+    instagramId: process.env.Insta || "_sks_bot_",
 
     /**
      * other details and parameters
      */
-    author: process.env.Author || "𝐏𝐢𝐤𝐚𝐁𝐨𝐭𝐳 🌚🌙",
-    packname: process.env.PackName || "𝕼𝖚𝖊𝖊𝖓 𝕬𝖓𝖞𝖆 𝖁2 𝕾𝖙𝖎𝖈𝖐𝖊𝖗 𝕻𝖆𝖈𝖐 🪀",
-    socialLink: process.env.Web || "https://github.com/PikaBotz",
-    groupLink: process.env.GcLink || "https://chat.whatsapp.com/E490r0wSpSr89XkCWeGtnX",
+    author: process.env.Author || "ㅤ𝗦𝗞𝗦 𝙱ᷤ𝙾ᷜ𝚃ᷤ 🌚🌙",
+    packname: process.env.PackName || "ig|_sks_bot_",
+    socialLink: process.env.Web || "sy4m.vercel.app",
+    groupLink: process.env.GcLink || "https://chat.whatsapp.com/DAD1FGg9dES0FpxtrUEheo",
     warns: Number(process.env.Warn_Limits) || 3,
     cooldown: Number(process.env.Cooldown_Timer_Seconds) || 5, // default cooldown time per command in seconds
-    mongoUrl: process.env.MongoDB || "YOUR_MONGODB_URL",
-    sessionId: process.env.SESSION_ID || "YOUR_SESSION_ID",
+    mongoUrl: process.env.MongoDB || "mongodb+srv://sksheroku81:hs5FGsQKJDBs6zBa@cluster0.qejuy.mongodb.net/",
+    sessionId: process.env.SESSION_ID || "session",
 
     /**
      * thumbnails and medias
      */
-    image_1: readFileSync('./lib/Assets/image_1.jpg'), // primary image
-    image_2: readFileSync('./lib/Assets/image_2.jpg'), // secondry image
-    imageUrl: "https://i.ibb.co/ZKKSZHT/Picsart-23-06-24-13-36-01-843.jpg", // image url, to replace to your url upload your image to https://imgbb.com
-    imageMentionUrl: "https://i.ibb.co/ZKKSZHT/Picsart-23-06-24-13-36-01-843.jpg", // image for mention reply audio media
-    aliveMedia: readFileSync("./lib/Assets/aliveMedia.mp4"),
-    menuMedia: readFileSync('./lib/Assets/menuMedia.mp4'),
-    ownerMentionMp3: readFileSync('./lib/Assets/ownerMentionMp3.mp3'),  // audio for mention reply audio media
+    image_1: readFileSync('https://cdn.ironman.my.id/i/55tpoo.jpg'), // primary image
+    image_2: readFileSync('https://cdn.ironman.my.id/i/55tpoo.jpg'), // secondry image
+    imageUrl: "https://cdn.ironman.my.id/i/55tpoo.jpg", // image url, to replace to your url upload your image to https://imgbb.com
+    imageMentionUrl: "https://cdn.ironman.my.id/i/55tpoo.jpg", // image for mention reply audio media
+    aliveMedia: readFileSync("https://files.catbox.moe/bonndh.mp4"),
+    menuMedia: readFileSync('https://cdn.ironman.my.id/i/1nbmvd.mp4'),
+    ownerMentionMp3: readFileSync('https://files.catbox.moe/oxncyb.mp4'),  // audio for mention reply audio media
 
     /**
      * core parameters and values
